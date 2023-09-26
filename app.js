a271ef73df3e806be26b706fcf2d1843920d5794
Sebastian Colon <sebastian.colon08@gmail.com>
2023-09-26 14:16:08 -0400

    done

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Register the location for handlebars partials here:
 
 // ...
@@ -19,7 +21,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers.hbs', { beersFromApi })
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then((beersFromApi) => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('random.hbs', { beersFromApi })
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/beer/:id', (req, res, next) => {
+
+  console.log("Params", req.params)
+  
+  punkAPI
+  .getBeer(req.params.id)
+  .then((beersFromApi) => {
+    // console.log('Beers from the database: ', beersFromApi)
+    res.render('random.hbs', { beersFromApi })
+  })
+  .catch(error => console.log(error));
+
+
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
